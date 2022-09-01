ae975b0d9cc843387f0cf5066f1dea7f5bcc8105
Mariano <marianoovelar200@gmail.com>
2022-09-01 18:09:44 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BienvenidoComponent } from './pages/bienvenido/bienvenido.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:BienvenidoComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'**',
+    component:ErrorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'practicas';
+  public promedio = 0;
+  public suma = 0;
+  public edadUno = 1;
+  public edadDos = 1;
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.Limpiar();
+  }
+
+  public Calcular(): void {
+    this.suma = this.edadUno + this.edadDos;
+    this.promedio = this.suma / 2;
+  }
+  public Limpiar(): void {
+    this.promedio = 0
+    this.suma = 0
+    this.edadUno = 1
+    this.edadDos = 1
+  }
 }
